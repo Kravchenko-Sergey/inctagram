fb02cc6b0143abedcc7ef0ae9f0fea0453e39bed
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-09-12 12:18:02 +0300

    feat: added page navigation

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/sign-in">Sign in</Link>
+        </li>
+        <li>
+          <Link href="/sign-up">Sign up</Link>
+        </li>
+        <li>
+          <Link href="/profile">Profile</Link>
+        </li>
+      </ul>
+    </main>
+  )
+}
