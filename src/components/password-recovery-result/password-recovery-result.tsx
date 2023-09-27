c039962c7deda00ce3334281e4d1a81b2dca41ad
kirillmorgunov <kirillmorgunov@mail.ru>
2023-09-27 15:54:38 +0400

    [IN-18] add translates for recovery-result page

--- src/components/password-recovery-result/password-recovery-result.tsx
+++ src/components/password-recovery-result/password-recovery-result.tsx
@@ -6,19 +6,22 @@ import { LinkExpiredIcon } from '@/assets/icons/link-expired'
 import { Button } from '@/components/button'
 import { Typography } from '@/components/typography'
 import { PATH } from '@/consts/route-paths'
+import { useTranslation } from '@/hooks/use-translation'
 
 const PasswordRecoveryResultPageComponent = memo(() => {
+  const { t } = useTranslation()
+
   return (
     <div className={s.root}>
       <div className={s.content}>
         <Typography variant={'h1'} className={s.title}>
-          Email verification link expired
+          {t.auth.emailExpired}
         </Typography>
         <Typography variant={'regular_text_16'} className={s.description}>
-          Looks like the verification link has expired. Not to worry, we can send the link again
+          {t.auth.expiredDescription}
         </Typography>
         <Button variant="link-btn" href={PATH.FORGOT_PASSWORD} fullWidth={true}>
-          Resend Link
+          {t.auth.resendLink}
         </Button>
       </div>
       <LinkExpiredIcon />
