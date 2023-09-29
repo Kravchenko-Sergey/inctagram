56facd19f9f7c9ec7367c82cf6277086977cfa0b
Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-09-29 14:28:36 +0300

    feat: add login.stories.tsx

---
+++ src/components/login/login.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Login } from './'
+
+import { LoginFormValues } from '@/schemas/loginSchema'
+
+const meta = {
+  title: 'Components/AUTH/Login',
+  component: Login,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Login>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    onGoogleAuth: () => console.log('Google auth clicked'),
+    onGithubAuth: () => console.log('GitHub auth clicked'),
+  },
+}
