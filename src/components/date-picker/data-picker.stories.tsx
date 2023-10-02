91a9cdc01b658220e92f269e77962f106fe8a437
pavelcardash <cardash@mail.ru>
2023-10-02 15:06:30 +0300

    done visial

--- src/components/date-picker/data-picker.stories.tsx
+++ src/components/date-picker/data-picker.stories.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { DataPicker, DatePickerProps } from '@/src/components/date-picker/data-picker'
+import { DataPicker, DatePickerProps } from '@/components/date-picker/data-picker'
 
 const meta = {
   title: 'Components/DataPicker',
@@ -16,6 +16,19 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   render: (args: DatePickerProps) => {
     const [startDate, setStartDate] = useState<Date | null>(new Date())
+
+    return <DataPicker setStartDate={setStartDate} startDate={startDate} />
+  },
+
+  args: {
+    setStartDate: () => {},
+    startDate: null,
+  },
+}
+export const Range: Story = {
+  render: (args: DatePickerProps) => {
+    const [startDate, setStartDate] = useState<Date | null>(new Date())
+    const [endDate, setEndDate] = useState<Date | null>(startDate?.getDate() + 5)
 
     return <DataPicker setStartDate={setStartDate} startDate={startDate} />
   },
