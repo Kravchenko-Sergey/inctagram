cb45157974ee166eae0221c92030670b8187c5dd
pavelcardash <cardash@mail.ru>
2024-01-05 13:00:01 +0300

    done tabs remove console

---
+++ src/pages/profile-settings/device/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { getSettingsTabLayout } from '@/components/layout/settings-tabs-layout/settings-tab-layout'
+
+const Device = () => {
+  return <>Device</>
+}
+
+Device.getLayout = getSettingsTabLayout
+export default Device
---
+++ src/pages/profile-settings/payments/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { getSettingsTabLayout } from '@/components/layout/settings-tabs-layout/settings-tab-layout'
+
+export const Payments = () => {
+  return <></>
+}
+
+Payments.getLayout = getSettingsTabLayout
+export default Payments
